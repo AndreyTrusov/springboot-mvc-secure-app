62f1c772d6bb815a18efa6a822be6134451f0184
aatrusov <trusovandrey.aleks@gmail.com>
2024-12-25 14:17:43 +0100

    initializing spring boot

---
+++ src/main/java/sk/project/springboot_mvc_secure_app/SpringbootMvcSecureAppApplication.java
@@ -0,0 +1,13 @@
+package sk.project.springboot_mvc_secure_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcSecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcSecureAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/project/springboot_mvc_secure_app/SpringbootMvcSecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.project.springboot_mvc_secure_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcSecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
